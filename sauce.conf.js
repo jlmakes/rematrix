7734398e8deb8430cf59ab6cd34f70b4bdef1c34
J.L. Makes <julianlloyd@gmail.com>
2017-09-22 04:04:57 +0200

    update custom browser launchers

--- sauce.conf.js
+++ sauce.conf.js
@@ -1,71 +1,81 @@
-module.exports = {
+const launchers = {}
 
-	// Evergreen
+;[
+	['iOS', '8.4', 'Safari', 'iPhone 6 Simulator'],
+	['iOS', '9.3', 'Safari', 'iPhone 6s Simulator'],
+	['iOS', '10.3', 'Safari', 'iPhone 6s Simulator'],
+	['Android', '4.4', 'Browser', 'Android Emulator'],
+	['Android', '5.0', 'Browser', 'Android Emulator'],
+	['Android', '5.1', 'Browser', 'Android Emulator'],
+	['Android', '6.0', 'Chrome', 'Android Emulator'],
+].forEach(config => {
+	const platform = config[0]
+	const version = config[1]
+	const browser = config[2]
+	const device = config[3]
 
-	sl_chrome: {
-		base: 'SauceLabs',
-		browserName: 'chrome',
-		platform: 'Windows 7',
-	},
+	const launcher = `sl_${platform}_${version}_${browser}`.replace(/[^a-z0-9]/gi, '_').toLowerCase()
 
-	sl_firefox: {
-		base: 'SauceLabs',
-		browserName: 'firefox',
-	},
+	launchers[launcher] = {
+		name: `${browser}, ${platform} ${version}`,
+		platformName: platform,
+		platformVersion: version,
+		browserName: browser,
+		deviceName: device,
+		deviceOrientation: 'portrait',
+		appiumVersion: '1.6.4',
+	}
+})
 
-	sl_mac_safari: {
-		base: 'SauceLabs',
-		browserName: 'safari',
-		platform: 'OS X 10.10',
-	},
+;[
+	['Windows 8.1', 'Internet Explorer', '11.0'],
+	['Windows 8', 'Internet Explorer', '10.0'],
+	['macOS 10.12', 'Safari', '10.0'],
+	['OS X 10.11', 'Safari', '9.0'],
+	['OS X 10.10', 'Safari', '8.0'],
+	['OS X 10.9', 'Safari', '7.0'],
+].forEach(config => {
+	const platform = config[0]
+	const browser = config[1]
+	const version = config[2]
 
-	// Internet Explorer
+	const launcher = `sl_${platform}_${browser}_${version}`.replace(/[^a-z0-9]/gi, '_').toLowerCase()
 
-	sl_ie_10: {
-		base: 'SauceLabs',
-		browserName: 'internet explorer',
-		platform: 'Windows 8',
-		version: '10',
-	},
+	launchers[launcher] = {
+		name: `${browser} ${version}, ${platform}`,
+		browserName: browser,
+		version,
+		platform,
+	}
+})
 
-	sl_ie_11: {
-		base: 'SauceLabs',
-		browserName: 'internet explorer',
-		platform: 'Windows 8.1',
-		version: '11',
-	},
+;['Chrome', 'Firefox', 'MicrosoftEdge'].forEach(browser => {
+	let legacy = 3
+	let alias = browser
 
-	sl_edge: {
-		base: 'SauceLabs',
-		browserName: 'MicrosoftEdge',
-		platform: 'Windows 10',
-	},
+	if (browser === 'MicrosoftEdge') {
+		legacy--
+		alias = 'Edge'
+	}
 
-	// Mobile
+	while (legacy >= 0) {
+		let postfix = (legacy > 0) ? `-${legacy}` : ''
+		const version = 'latest' + postfix
 
-	sl_ios_8: {
-		base: 'SauceLabs',
-		browserName: 'iphone',
-		version: '8.4',
-	},
+		const launcher = `sl_win10_${browser}_latest${postfix}`.replace(/-/g, '_').toLowerCase()
+		launchers[launcher] = {
+			name: `${alias} ${version}, Windows 10`,
+			browserName: browser,
+			version,
+			platform: 'Windows 10',
+		}
 
-	sl_ios_9: {
-		base: 'SauceLabs',
-		browserName: 'iphone',
-		version: '9.3',
-	},
+		legacy--
+	}
+})
 
-	sl_android_4: {
-		base: 'SauceLabs',
-		browserName: 'Browser',
-		platform: 'Android',
-		version: '4.4',
-		device: 'Android Emulator',
-	},
-
-	sl_android_5: {
-		base: 'SauceLabs',
-		browserName: 'android',
-		version: '5.1',
-	},
+for (const launcher in launchers) {
+	launchers[launcher].base = 'SauceLabs'
 }
+
+module.exports = launchers
