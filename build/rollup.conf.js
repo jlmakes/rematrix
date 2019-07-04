b23dfb38505b7552543d3270159212a29ca56f63
Julian Lloyd <julianlloyd@gmail.com>
2019-07-04 12:51:04 -0700

    Upgrade rollup config

--- build/rollup.conf.js
+++ build/rollup.conf.js
@@ -2,11 +2,10 @@ import buble from 'rollup-plugin-buble'
 import { standard as banner } from './rollup.conf.banner'
 
 export default {
-  banner,
   input: 'src/index.js',
   plugins: [buble()],
   output: [
-    { file: 'dist/rematrix.js', format: 'umd', name: 'Rematrix' },
-    { file: 'dist/rematrix.es.js', format: 'es' },
+    { banner, file: 'dist/rematrix.js', format: 'umd', name: 'Rematrix' },
+    { banner, file: 'dist/rematrix.es.js', format: 'es' },
   ],
 }
